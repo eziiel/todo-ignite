aaadc3b3979a42c179734c2514a22c3796fe1259
eziiel <eziielp10@hotmail.com>
2022-12-10 23:28:16 -0300

    reformed taks

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import "./global.css"
 import Header from "./components/header"
 import FormToDo from "./components/form"
-import InfoTasks from "./components/infoTasks"
-import Tasks from "./components/tasks"
 
 function App() {
   return (
     <div>
       <Header />
       <FormToDo />
-      <InfoTasks />
-      <Tasks />
     </div>
   )
 }
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,20 +1,87 @@
 import style from "./formTask.module.css"
 import { PlusCircle } from "phosphor-react"
+import { ClipboardText } from "phosphor-react"
+
+import React, { ChangeEvent, FormEvent } from "react"
+import Task from "./task"
+import InfoTasks from "./infoTasks"
+
+function VoidTasks () {
+  return (
+    <div className = {style.voidTasks}>
+      <ClipboardText size={50}/>
+      <strong>Você ainda não possui tarefas cadastradas</strong>
+      <span>Crie tarefas e organize seus itens a fazer</span>
+    </div>
+  )
+}
+
+interface Tasks {
+  id: number,
+  task: string,
+  checked: false
+}
 
 export default function FormToDo () {
+  const [tasks, setTasks] = React.useState<Tasks  []>([])
+  const [task, setTask] = React.useState("")
+  
+  const handleWriteTaskChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTask(event.target.value)
+  }
+
+  const handleCreatTask = (event: FormEvent) => {
+    event.preventDefault()
+    
+    let newTask:Tasks = {
+      id: tasks.length + 1,
+      task: task,
+      checked: false
+    }
+    console.log(newTask.id)
+    setTasks([...tasks, newTask])
+  }
+
+
   return(
-    <form className={style.FormNewTask}>
-      <input 
-        className={style.newTask}
-        type="text" 
-        placeholder="Adicione uma nova tarefa" 
-      />
-      <button type="submit"
-        className={style.buttonAddTask}
+    <main className={style.main}>
+      <form className={style.FormNewTask}
+        onSubmit={handleCreatTask}
       >
-        criar
-        <PlusCircle size={20} />
-      </button>
-    </form>
+        <input 
+          value={task}
+          onChange={handleWriteTaskChange}
+          className={style.newTask}
+          type="text" 
+          placeholder="Adicione uma nova tarefa" 
+        />
+        <button type="submit"
+          className={style.buttonAddTask}
+        >
+          criar
+          <PlusCircle size={20} />
+        </button>
+      </form>
+
+      <InfoTasks />
+
+      <section className = {style.MainTasks}>
+        {
+          tasks.length != 0 ?
+          <ul className = {style.listTaskes}>
+            {tasks.map(({ id, checked, task}) => (
+              <Task 
+              key={id}
+              id ={id}
+              checked = {checked}
+              task ={task}
+              />
+            ))}
+          </ul>
+          :
+            <VoidTasks />
+        }
+      </section>
+    </main>
   )
 }
